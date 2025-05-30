c0e956e7bd66c2b2cb77f2a8b125acc3df2cdd01
Lastrevio112 <stefanzingzing@yahoo.com>
2025-05-30 18:40:50 +0300

    Added test query in test_conn.py to check whether tables were created properly in PostgreSQL.

--- test_conn.py
+++ test_conn.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv()  # loads SUPABASE_* into os.environ
 
@@ -16,3 +17,19 @@ with engine.begin() as conn:
     # wrap your SQL in sqlalchemy.text()
     version = conn.execute(text("SELECT version()")).scalar_one()
     print("🎉 Connected to:", version)
+
+
+# Testing an SQL query
+sql = text("""
+    SELECT 
+      table_name,
+      column_name,
+      data_type
+    FROM information_schema.columns
+    WHERE table_schema = 'public'
+    ORDER BY table_name, ordinal_position;
+""")
+
+#store result in pandas dataframe
+df = pd.read_sql(sql, con=engine)
+print(df.to_string())
